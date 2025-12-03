84d9beb06e0993a02b5374abff00159d43b0d4ed
saribence <saribence10@gmail.com>
2025-12-03 10:16:11 +0100

    Station.java letrehozása

---
+++ src/main/java/com/example/javagyakszi/JavagyaksziApplication.java
@@ -0,0 +1,13 @@
+package com.example.javagyakszi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavagyaksziApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavagyaksziApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javagyakszi/JavagyaksziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javagyakszi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavagyaksziApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
